e6156d3c9cb07d841688e5425687feb865bbe2be
Yang Song <yang.song@datadoghq.com>
2023-01-17 14:27:29 -0500

    Add support for parsing JSON requests

--- src/main/java/com/datadog/controller/OTLPController.java
+++ src/main/java/com/datadog/controller/OTLPController.java
@@ -1,5 +1,6 @@
 package com.datadog.controller;
 
+import com.googlecode.protobuf.format.JsonFormat;
 import io.opentelemetry.proto.metrics.v1.MetricsData;
 import io.opentelemetry.proto.trace.v1.TracesData;
 import java.io.BufferedInputStream;
@@ -14,17 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class OTLPController {
 
-  @RequestMapping(method = RequestMethod.POST, value = "/v1/traces")
-  public void otlpTraces(InputStream inputStream) throws IOException {
+  @RequestMapping(method = RequestMethod.POST, value = "/v1/traces", headers = "content-type=application/x-protobuf")
+  public void otlpProtoTraces(InputStream inputStream) throws IOException {
     BufferedInputStream in = new BufferedInputStream(inputStream);
     TracesData tracesData = TracesData.parseFrom(in.readAllBytes());
     log.info("traceData:" + tracesData);
   }
 
-  @RequestMapping(method = RequestMethod.POST, value = "/v1/metrics")
-  public void otlpMetrics(InputStream inputStream) throws IOException {
+  @RequestMapping(method = RequestMethod.POST, value = "/v1/metrics", headers = "content-type=application/x-protobuf")
+  public void otlpProtoMetrics(InputStream inputStream) throws IOException {
     BufferedInputStream in = new BufferedInputStream(inputStream);
     MetricsData metricsData = MetricsData.parseFrom(in.readAllBytes());
     log.info("metricsData:" + metricsData);
+  }
+
+  private final JsonFormat jsonFormat = new JsonFormat();
+
+  @RequestMapping(method = RequestMethod.POST, value = "/v1/traces", headers = "content-type=application/json")
+  public void otlpJsonTraces(InputStream inputStream) throws IOException {
+      TracesData.Builder builder = TracesData.newBuilder();
+      jsonFormat.merge(inputStream, builder);
+      log.info("traceData:" + builder.build());
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/v1/metrics", headers = "content-type=application/json")
+  public void otlpJsonMetrics(InputStream inputStream) throws IOException {
+      MetricsData.Builder builder = MetricsData.newBuilder();
+      jsonFormat.merge(inputStream, builder);
+      log.info("metricsData:" + builder.build());
   }
 }
